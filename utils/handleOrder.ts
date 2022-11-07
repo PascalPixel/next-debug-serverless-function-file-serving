cf9d720cbf920937da9f6a62571858006d4e0c5a
Pascal Pixel <x@pascalpixel.com>
2022-11-07 18:51:32 +0000

    less log

--- utils/handleOrder.ts
+++ utils/handleOrder.ts
@@ -3,9 +3,8 @@ import getFileBase64 from "./getFileBase64";
 const handleOrder = async () => {
   try {
     const zipFile = await getFileBase64("hello.zip");
-    console.log(zipFile);
     const jsonFile = await getFileBase64("hello.json");
-    console.log(jsonFile);
+    console.log({ zipFile, jsonFile });
   } catch (e) {
     console.error(e);
   }
